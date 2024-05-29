0213392c7cdb9c23e2ea358bb648f2b414bce101
gpratoe <gaston.prato@mi.unc.edu.ar>
2024-05-29 13:49:39 -0300

    hellous

--- Net.cc
+++ Net.cc
@@ -2,14 +2,22 @@
 #define NET
 
 #include <string.h>
+#include <list>
 #include <omnetpp.h>
 #include <packet_m.h>
 
 using namespace omnetpp;
 
+struct tableEntry{
+    int node;
+    int distance;
+    std::string interface;
+};
+
 class Net: public cSimpleModule {
 private:
-
+    cMessage *initHelloEvent;
+    std::vector<struct tableEntry> nodeTable;
 public:
     Net();
     virtual ~Net();
@@ -30,6 +38,8 @@ Net::~Net() {
 }
 
 void Net::initialize() {
+    initHelloEvent = new cMessage("InitHello");
+    scheduleAt(simTime() + 0, initHelloEvent);
 }
 
 void Net::finish() {
@@ -39,9 +49,26 @@ void Net::handleMessage(cMessage *msg) {
 
     // All msg (events) on net are packets
     Packet *pkt = (Packet *) msg;
-
+    if(msg == initHelloEvent){ //Send all the hello´s to the neighbours
+        for(unsigned int i=0; i<gateSize("toLnk$o"); ++i){
+            Packet *hello = new Packet("Hello", 1); //1 is the kind of "Hello"
+            hello->setSource(this->getParentModule()->getIndex());
+            send(hello, "toLnk$o", i);
+        }
+        
+        
+    }
+    else if (pkt->getKind() == 1){ //Ensure the hello is send once and handle it
+        std::string outGate = "toLnk$o[" + std::to_string(pkt->getArrivalGate()->getIndex()) + "]";
+        struct tableEntry tabEnt = {pkt->getSource(), 1, outGate};
+        nodeTable.push_back(tabEnt); //Add to the routing table the neighbour
+        
+        EV << nodeTable[0].node << " vecino de " << this->getParentModule()->getIndex();
+        delete pkt;
+        this->bubble("hello dropped");
+    }
     // If this node is the final destination, send to App
-    if (pkt->getDestination() == this->getParentModule()->getIndex()) {
+    else if (pkt->getDestination() == this->getParentModule()->getIndex()) {
         send(msg, "toApp$o");
     }
     // If not, forward the packet to some else... to who?
